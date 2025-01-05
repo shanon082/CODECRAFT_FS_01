b0c28deb975f03e8355d300636b59ee4d38d1a32
shanon cymon <shanonsimon082@gmail.com>
2025-01-05 20:46:07 +0300

    Initial commit: CODECRAFT_FS_01

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $username = 'root';
 $password = '';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
