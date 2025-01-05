b46d472e06efd36183f784d2672e3a108143404a
shanon cymon <shanonsimon082@gmail.com>
2025-01-05 20:32:20 +0300

    Initial commit: CODECRAFT_FS_01

--- db.php
+++ db.php
@@ -5,9 +5,9 @@ $username = 'root';
 $password = '';
 
 try{
-    $conn = new PDO('mysql:host=$host; dbname = $dbname', $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+    $conn = new mysqli($host, $username, $password, $dbname);
+    
+} catch(Exception $e) {
     echo 'connection failed: '. $e->getMessage();
 }
 
